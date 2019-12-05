1a3ff3b9be3bc59833eab4bc59888304189aa247
Benjamin Kao <r07945005@ntu.edu.tw>
2019-12-05 19:23:58 +0800

    Create install_cuda8_ubuntu1604.sh

---
+++ install_cuda8_ubuntu1604.sh
@@ -0,0 +1,11 @@
+wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
+sudo dpkg -i cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64.deb
+sudo apt-get update
+sudo apt-get -y install cuda
+
+echo 'export CUDA_HOME=/usr/local/cuda' >> ~/.bashrc
+echo 'export PATH=$PATH:$CUDA_HOME/bin' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=$CUDA_HOME/lib64' >> ~/.bashrc
+source ~/.bashrc
+
+# after install cuda, one should reboot the computer
