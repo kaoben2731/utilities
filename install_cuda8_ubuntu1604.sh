1969eecef28fc7e80f8335422fd03473545ac873
Benjamin Kao <r07945005@ntu.edu.tw>
2019-12-05 23:56:46 +0800

    Update install_cuda8_ubuntu1604.sh

--- install_cuda8_ubuntu1604.sh
+++ install_cuda8_ubuntu1604.sh
@@ -1,5 +1,5 @@
-wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64.deb
+wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
+sudo dpkg -i cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
 sudo apt-get update
 sudo apt-get -y install cuda
 
