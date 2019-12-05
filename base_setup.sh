d3cd84c56d93bd3401b9c1e664fff0446bf6ad15
Benjamin Kao <r07945005@ntu.edu.tw>
2019-12-05 23:45:23 +0800

    Update base_setup.sh

--- base_setup.sh
+++ base_setup.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # install some program
-sudo apt install vim
-sudo apt-get install htop
-sudo apt-get install curl
+sudo apt install -y vim
+sudo apt-get install -y htop
+sudo apt-get install -y curl
 
