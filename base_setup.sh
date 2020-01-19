1d4cc10adb5459a25686bea46f575de078b7e7a0
Benjamin Kao <r07945005@ntu.edu.tw>
2020-01-19 18:35:23 +0800

    Update base_setup.sh

--- base_setup.sh
+++ base_setup.sh
@@ -8,5 +8,6 @@ sudo apt install -y vim
 sudo apt-get install -y htop
 sudo apt-get install -y curl
 sudo apt-get install -y net-tools
+sudo apt-get install -y zip unzip
 
 sudo curl https://esd.mathworks.com/R2019b/R2019b/installers/web/matlab_R2019b_glnxa64.zip?__gda__=1579489241_09812a907ad7ff839e5af21ec217804e&dl_id=prh03PdK&ext=.zip
