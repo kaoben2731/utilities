0561b95f047c3d5b1a2b30357f667324ac7fa8ff
Benjamin Kao <r07945005@ntu.edu.tw>
2020-01-26 17:46:41 +0800

    Update base_setup.sh

--- base_setup.sh
+++ base_setup.sh
@@ -9,3 +9,8 @@ sudo apt-get install -y htop
 sudo apt-get install -y curl
 sudo apt-get install -y net-tools
 sudo apt-get install -y zip unzip
+sudo apt-get install -y tmux
+sudo apt-get install -y openssh-server
+sudo systemctl status ssh
+sudo ufw allow ssh
+
